d044c11b31c75311b67c782e1dadb0c826e8ac31
sasi <sasi@cubera.co>
2023-03-29 15:56:58 +0530

    Chapter02-Igniting-our-app

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "This is H1 Tag"),
