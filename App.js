f19ab36a37612a69ae450e40ca8669b756a1696b
sasi <sasi@cubera.co>
2023-04-03 17:34:50 +0530

    Chapter04 - Working on same branch

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ const HeadingComponent = () => {
   return (
     <div id="container">
       <h1>Talk is cheap - Show me the Code</h1>
+      <h2>Just trying to work on same branch and pushing my changes to Git</h2>
     </div>
   );
 };
