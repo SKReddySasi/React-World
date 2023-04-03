fce94c7b936145987f359681d16602175550364d
sasi <sasi@cubera.co>
2023-04-03 17:29:15 +0530

    Chapter04- Talk is cheap, Show me the Code

--- App.js
+++ App.js
@@ -1,24 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => (
-  <div>
-    <h1 className="heading" tabIndex="5">
-      Namaste React Using JSX
-    </h1>
-  </div>
-);
 const HeadingComponent = () => {
   return (
     <div id="container">
-      <Title />
-      <h1>This Is Functional Component</h1>
-      <h2>In this Episode We have Learned</h2>
-      <ul>
-        <li><b>JSX</b> - HTML-like or XML-like syntax </li>
-        <li><b>Component Composition</b> - A "Component" inside another "Component"  is known as Component Composition.</li>
-        <li><b>React Fragment</b> - Behaves like a Empty Tag</li>
-      </ul>
+      <h1>Talk is cheap - Show me the Code</h1>
     </div>
   );
 };
