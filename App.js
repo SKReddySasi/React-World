ec9b7ccd7e807d46a64636fff560e29853f924a3
sasi <sasi@cubera.co>
2023-04-10 15:28:47 +0530

    Added Header

--- App.js
+++ App.js
@@ -1,15 +1,61 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const HeadingComponent = () => {
+const Header = () => {
   return (
-    <div id="container">
-      <h1>Talk is cheap - Show me the Code</h1>
-      <h2>Just trying to work on same branch and pushing my changes to Git</h2>
+    <div className="header">
+      <img
+        className="Logo"
+        src="https://lh3.googleusercontent.com/Em7AHf7XBH_RtGfCBVXz9RH8SM_pHkj3xPP-yd3cRguY1_Jc8fmqgx6WxnvGVyPV5xs5gL3HCD0FCuv6Xo4CwoY6ak4"
+        alt="Logo"
+      />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurentCard = () => {
+  return <div className="restaurent-Card"></div>;
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">
+        <input type="text" />
+      </div>
+      <div className="restaurent-container">
+        <RestaurentCard />
+      </div>
+    </div>
+  );
+};
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h1>This is Footer</h1>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="AppLayout">
+      <Header />
+      <Body />
+      <Footer />
     </div>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
